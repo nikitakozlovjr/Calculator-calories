b27c7cc1ad152d3a5c8c7605f9f40f26f84764a1
nikitakozlovjr <nikita3726898@gmail.com>
2024-01-20 23:46:32 +0300

    Переименован файл с коэфицентами для расчета

---
+++ src/coefficients.js
@@ -0,0 +1,14 @@
+const genderConfig = { // dispatch by key (gender data)
+  man: 5,
+  women: -161,
+};
+
+const activityConfig = { // dispatch by key (active data)
+  min: 1.2,
+  low: 1.375,
+  average: 1.55,
+  high: 1.725,
+  huge: 1.9,
+};
+
+export { genderConfig, activityConfig };
